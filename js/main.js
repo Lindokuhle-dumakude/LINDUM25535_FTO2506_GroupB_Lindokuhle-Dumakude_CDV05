c6cf01bee97289f28246581b4566dccc44f0b8bc
unknown <lindokuhle.dumakude11@gmail.com>
2025-11-23 15:16:58 +0200

    update contact detail styles

--- js/main.js
+++
@@ -1,5 +0,0 @@
-document.querySelector("#contactForm").addEventListener("submit", (e) => {
-  e.preventDefault();
-  alert("Thank you for reaching out! I'll get back to you soon.");
-  e.target.reset();
-});
